5cc632c9de31d39dd058b5a510b5f9fff460f467
Gaurav K. Singh <gaurav.singh@ondotsystems.com>
2021-06-14 16:42:59 +0530

    Added Apis

---
+++ ipl-dashboard/src/main/java/com/xiffox/ipl/controller/TeamController.java
@@ -0,0 +1,27 @@
+package com.xiffox.ipl.controller;
+
+import com.xiffox.ipl.model.Team;
+import com.xiffox.ipl.repository.MatchRepository;
+import com.xiffox.ipl.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
+        return team;
+    }
+
+}
---
+++ ipl-dashboard/src/main/java/com/xiffox/ipl/repository/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.xiffox.ipl.repository;
+
+import com.xiffox.ipl.model.Team;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends CrudRepository<Team, Long> {
+
+    Team findByTeamName(String teamName);
+}
