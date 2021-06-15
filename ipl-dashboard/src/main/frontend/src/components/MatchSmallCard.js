cfc9623dc61d5259841b6707142354c317e312fd
Gaurav K. Singh <gaurav.singh@ondotsystems.com>
2021-06-15 16:31:54 +0530

    React app with team page

--- ipl-dashboard/src/main/frontend/src/components/MatchSmallCard.js
+++ ipl-dashboard/src/main/frontend/src/components/MatchSmallCard.js
@@ -1,10 +1,14 @@
 import { React } from 'react';
 
 
-export const MatchSmallCard = () => {
+export const MatchSmallCard = ({match}) => {
+
+  if(!match) return null;
+  
   return (
     <div className="MatchSmallCard">
-      <p>Match Small Card</p>
+      <p>{match.team1} vs {match.team2}</p>
+
     </div>
   );
 }
